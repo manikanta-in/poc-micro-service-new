ff63e95691446c25e4c0cd5454c4e339b473e4be
Manikanta Kuchipudi <mkuchipudi@modeln.com>
2019-12-08 14:40:06 +0530

    local

--- src/main/java/com/poc/ms/repo/package-info.java
+++
@@ -1,1 +0,0 @@
-package com.poc.ms.repo;
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -3,6 +3,7 @@
 CREATE TABLE IF NOT EXISTS tbl_user (
   id SERIAL PRIMARY KEY ,
   username varchar(25) NOT NULL,
+  username1 varchar(75),
   password varchar(75) NOT NULL,
   salary int NOT NULL,
   age int NOT NULL
